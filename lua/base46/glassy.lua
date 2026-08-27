5aae4f1292ca8d568eb99967c6904b4379ee0f01
Alex Rosas <rosasrias82@gmail.com>
2026-08-27 15:49:42 -0500

    feat(ui): expand trasnparency support
    - add transparent hl for core Neovim ui
    - add NvimTree, Telescope and completion transparency
    - add Wichkey, Noice and notifications hl
    - add Rename, Alpha and bufferline support
    - add BlinkCmp transparency and borders
    - unify floating window and separator borders
    - remove duplicated hl definitions

--- lua/base46/glassy.lua
+++ lua/base46/glassy.lua
@@ -1,75 +1,232 @@
 local colors = require("base46").get_theme_tb "base_30"
 
 local M = {
+  -- Core / Neovim
   NvimTreeWinSeparator = {
     fg = colors.one_bg2,
     bg = "NONE",
   },
 
+  WinSeparator = {
+    fg = colors.one_bg2,
+    bg = "NONE",
+  },
+
+  TerminalSeparator = {
+    fg = colors.one_bg2,
+    bg = "NONE",
+  },
+
+  -- Telescope
   TelescopeResultsTitle = {
     fg = colors.black,
     bg = colors.blue,
   },
+
+  TelescopeBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  TelescopePromptBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  TelescopePreviewBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  TelescopeResultsBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  -- Completion
+  CmpBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  CmpDocBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  BlinkCmpMenuBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  BlinkCmpDocBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
+
+  -- Noice
+  NoiceCmdlinePopupBorder = {
+    fg = colors.grey,
+    bg = "NONE",
+  },
 }
 
--- for hl groups which need bg = "NONE" only!
-local hl_groups = {
-  "NormalFloat",
+-- Highlights that only require a transparent background.
+local transparent_groups = {
+  -- Core
   "Normal",
+  "NormalFloat",
   "Folded",
+  "FoldColumn",
+  "LineNr",
+  "CursorColumn",
+  "ColorColumn",
+  "SignColumn",
+  "CursorLine",
+  "MsgArea",
+  "WinBar",
+  "WinBarNC",
+
+  -- NvimTree
   "NvimTreeNormal",
   "NvimTreeNormalNC",
   "NvimTreeCursorLine",
+  "NvimTreeLineNr",
+
+  -- Terminal
+  "TerminalNormal",
+
+  -- Telescope
   "TelescopeNormal",
   "TelescopePrompt",
   "TelescopeResults",
   "TelescopePromptNormal",
   "TelescopePromptPrefix",
-  "CursorLine",
+  "TelescopeSelection",
+
+  -- Completion
   "Pmenu",
+  "CmpPmenu",
+  "CmpNormal",
+  "CmpItemAbbr",
+  "CmpItemAbbrDeprecated",
+  "CmpItemMenu",
+
   "BlinkCmpMenu",
   "BlinkCmpDoc",
   "BlinkCmpSignatureHelp",
-  "CmpPmenu",
-  "CmpDoc",
-  "WinBar",
-  "WinBarNC",
+
+  -- WhichKey
+  "WhichKey",
+  "WhichKeyGroup",
+  "WhichKeyDesc",
+  "WhichKeyFloat",
+
+  -- Treesitter
+  "TreesitterContext",
+
+  -- Bufferline / tabline
+  "BuffLineTree",
+  "TabLine",
+  "TabLineFill",
+
+  -- Saga
+  "SagaNormal",
+  "SagaBorder",
+
+  -- Rename
+  "RenameNormal",
+  "RenameBorder",
+
+  -- Alpha
+  "AlphaHeader",
+  "AlphaMessage",
+  "AlphaLabel",
+  "AlphaIcon",
+  "AlphaFooter",
+
+  -- Noice
+  "NoiceMini",
+  "NoiceCmdlinePopup",
+  "NoiceCmdlinePopupBorder",
+  "NoiceCmdlinePopupBorderSearch",
+  "NoiceCmdlinePopupTitle",
+
+  -- Notifications
+  "NotifyBackground",
+  "NotifyINFOBorder",
+  "NotifyWARNBorder",
+  "NotifyERRORBorder",
+  "NotifyDEBUGBorder",
+  "NotifyTRACEBorder",
+  "NotifyLogTime",
+
+  "NotifyERRORIcon",
+  "NotifyWARNIcon",
+  "NotifyINFOIcon",
+  "NotifyDEBUGIcon",
+  "NotifyTRACEIcon",
+
+  "NotifyERRORTitle",
+  "NotifyWARNTitle",
+  "NotifyINFOTitle",
+  "NotifyDEBUGTitle",
+  "NotifyTRACETitle",
+
+  "NotifyERRORBody",
+  "NotifyWARNBody",
+  "NotifyINFOBody",
+  "NotifyDEBUGBody",
+  "NotifyTRACEBody",
 }
 
-for _, groups in ipairs(hl_groups) do
-  M[groups] = {
+for _, group in ipairs(transparent_groups) do
+  M[group] = {
     bg = "NONE",
   }
 end
 
-M.TelescopeBorder = {
-  fg = colors.grey,
+-- Completion match
+M.CmpItemAbbrMatch = {
+  bg = "NONE",
+  bold = true,
+}
+
+-- Notification borders retain semantic colors while remaining transparent.
+M.NotifyINFOBorder = {
+  fg = colors.blue,
   bg = "NONE",
 }
 
-M.TelescopePromptBorder = {
-  fg = colors.grey,
+M.NotifyWARNBorder = {
+  fg = colors.yellow,
   bg = "NONE",
 }
 
-M.CmpDocBorder = {
-  bg = "none",
-  fg = colors.grey,
+M.NotifyERRORBorder = {
+  fg = colors.red,
+  bg = "NONE",
 }
 
-M.BlinkCmpDocBorder = {
-  bg = "none",
-  fg = colors.grey,
+M.NotifyDEBUGBorder = {
+  fg = colors.blue,
+  bg = "NONE",
 }
 
-M.BlinkCmpMenuBorder = {
-  bg = "none",
-  fg = colors.grey,
+M.NotifyTRACEBorder = {
+  fg = colors.green,
+  bg = "NONE",
 }
 
-M.BlinkCmpDocBorder = {
-  bg = "none",
+M.NotifyBackground = {
+  fg = "NONE",
+  bg = "NONE",
+}
+
+-- Generic floating window border.
+M.FloatBorder = {
   fg = colors.grey,
+  bg = "NONE",
 }
 
 return M
